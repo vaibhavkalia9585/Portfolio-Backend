fded6294f73da4d42276bb6e10ffc505ea7c3d3e
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-11-02 00:38:57 -0400

    updated

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); /
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Import routes
+
 const contactRoutes = require('./routes/contacts');
 const userRoutes = require('./routes/users');
 
-// Define routes
+
 app.use('/api/contacts', contactRoutes);
 app.use('/api/users', userRoutes);
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { // Use the environment variable for the connection string
+
+mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -30,12 +30,11 @@ mongoose.connect(process.env.MONGODB_URI, { // Use the environment variable for 
     console.log('MongoDB connection error:', err);
 });
 
-// Index route
 app.get('/', (req, res) => {
     res.send('Welcome to Portfolio Backend API');
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
